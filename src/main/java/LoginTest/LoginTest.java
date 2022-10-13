f41d8ccfbfb2bbad2ff3dd1357005062a607586e
Apurva2022 <113265745+Apurva2022@users.noreply.github.com>
2022-10-13 14:17:26 -0500

    first commit

---
+++ src/main/java/LoginTest/LoginTest.java
@@ -0,0 +1,27 @@
+package LoginTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+ 
+	public static void main(String[] args) {
+		
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Raj\\Desktop\\Selenium\\crm\\src\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	 
+}
